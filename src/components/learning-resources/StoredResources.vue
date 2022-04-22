79106a3055de11616bf411d4cce23cd4a8ec56e6
Alfred Tuva <muindetuva@gmail.com>
2022-04-22 22:27:42 +0300

    Add the first components

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <ResourceItem
+      v-for="res in resources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    />
+  </ul>
+</template>
+
+<script>
+import ResourceItem from './ResourceItem.vue';
+
+export default {
+  props: ['resources'],
+  components: { ResourceItem },
+};
+</script>
